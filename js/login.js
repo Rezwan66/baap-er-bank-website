ca61e3134333da58e4677919e6830edae51b322b
Rezwan66 <shaikhrezwan66@gmail.com>
2023-08-15 19:50:21 +0600

    added event handler on submit btn to check validity of email and password

---
+++ js/login.js
@@ -0,0 +1,27 @@
+// check email and password when submit btn clicked
+// STEP 1: added click event handler with the submit btn
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // console.log('submit btn clicked');
+    // STEP 2: access the email and password inside the input fields
+        /*
+            2.a Set an id on the html element 
+            2.b Get the element
+            2.c Get the value from the element
+        */
+    // we always need to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+    // console.log(email, password);
+
+    /* 
+        DANGER: NEVER VERIFY EMAIL AND PASSWORD ON THE CLIENT SIDE, WE MUST DO IT ON THE SERVER SIDE!!! 
+    */
+    // STEP 3: verify email and password (and check if valid user or not)
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        console.log('valid user');
+    }else{
+        console.log('invalid user');
+    }
+})
